b045c51023156d0785a08301e0a736bec3a8e6a6
WegaSilver <109556907+TonyBoyDeFi@users.noreply.github.com>
2023-04-07 12:39:46 +0300

    Update index.tsx

--- apps/web/src/views/Home/index.tsx
+++ apps/web/src/views/Home/index.tsx
@@ -54,28 +54,28 @@ const Home: React.FC<React.PropsWithChildren> = () => {
       <style jsx global>
         {`
           #home-1 .page-bg {
-            background: linear-gradient(139.73deg, #e6fdff 0%, #f3efff 100%);
+            background: linear-gradient(139.73deg, #f59a40 0%, #ff9124 100%);
           }
           [data-theme='dark'] #home-1 .page-bg {
             background: radial-gradient(103.12% 50% at 50% 50%, #21193a 0%, #191326 100%);
           }
           #home-2 .page-bg {
-            background: linear-gradient(180deg, #ffffff 22%, #d7caec 100%);
+            background: linear-gradient(139.73deg, #f59a40 0%, #ff9124 100%);
           }
           [data-theme='dark'] #home-2 .page-bg {
-            background: linear-gradient(180deg, #09070c 22%, #201335 100%);
+            background: linear-gradient(139.73deg, #f59a40 0%, #ff9124 100%);
           }
           #home-3 .page-bg {
-            background: linear-gradient(180deg, #6fb6f1 0%, #eaf2f6 100%);
+            background: linear-gradient(139.73deg, #f59a40 0%, #ff9124 100%);
           }
           [data-theme='dark'] #home-3 .page-bg {
-            background: linear-gradient(180deg, #0b4576 0%, #091115 100%);
+            background: linear-gradient(139.73deg, #f59a40 0%, #ff9124 100%);
           }
           #home-4 .inner-wedge svg {
-            fill: #d8cbed;
+            fill: #f18312;
           }
           [data-theme='dark'] #home-4 .inner-wedge svg {
-            fill: #201335;
+            fill: #f18312;
           }
         `}
       </style>
